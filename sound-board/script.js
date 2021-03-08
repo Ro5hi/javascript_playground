5d155ee9d0d55a371abfb27dda6aa1485b7d2bc1
Ro5hi <slen.detablan@gmail.com>
2021-03-08 15:21:14 -0700

    moved files and cleared base files for zoom effect

---
+++ sound-board/script.js
@@ -0,0 +1,32 @@
+const sounds = [
+    'open hat',
+    'kick',
+    'snare',
+    'percussion',
+    'closed hat'
+]
+
+
+sounds.forEach((sound) => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+
+    btn.innerText = sound;
+
+    btn.addEventListener("click", () => {
+        stopSongs();
+
+        document.getElementById(sound).play();
+    });
+
+    document.body.appendChild(btn);
+});
+
+function stopSongs() {
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0;
+    });
+}
